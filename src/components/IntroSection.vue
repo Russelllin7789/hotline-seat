c16013fc1ff26958a04760970dc60fb239b4f951
Russell Lin <superbr7789@gmail.com>
2023-07-01 08:46:03 +0800

    correct some styles

--- src/components/IntroSection.vue
+++ src/components/IntroSection.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="w-full p-4 my-16">
-    <h1 class="text-white font-bold text-center mb-8">熱線募款感恩會</h1>
+    <h1 class="text-3xl text-white font-bold text-center mb-8">
+      熱線募款感恩會
+    </h1>
     <div class="flex items-center justify-center">
       <div class="max-w-[350px] rounded-lg overflow-hidden">
         <img src="/logo.jpeg" alt="key-feature" class="w-full object-cover" />
@@ -19,7 +21,7 @@
             <div class="text-xl text-white font-bold hover:text-[#000dff]">
               #乘風破浪
             </div>
-            <div class="text-xl text-gray font-bold">#又破又浪</div>
+            <div class="text-xl text-[#c8c8c8] font-bold">#又破又浪</div>
           </li>
         </ul>
       </div>
